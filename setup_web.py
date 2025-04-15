d5dfc979324458d251d5339ff81fb8c39acca936
AbisaiHdz06 <Al03095697@tecmilenio.mx>
2025-04-14 20:44:23 -0600

    Update setup_web.py

--- setup_web.py
+++ setup_web.py
@@ -1,26 +1,31 @@
 import os
 import subprocess
-import stat
 
 def run(cmd):
     print(f"Ejecutando: {cmd}")
     subprocess.run(cmd, shell=True, check=True)
 
 def instalar_apache():
-    # Detectar distribución
-    if os.path.exists("/etc/debian_version"):
+    # Detectar distro para usar el gestor correcto
+    os_release = ""
+    if os.path.exists("/etc/os-release"):
+        with open("/etc/os-release") as f:
+            os_release = f.read()
+
+    if "debian" in os_release or "ubuntu" in os_release:
         run("sudo apt update")
         run("sudo apt install -y apache2")
         run("sudo systemctl start apache2")
         run("sudo systemctl enable apache2")
-    elif os.path.exists("/etc/redhat-release"):
+
+    elif "amzn" in os_release or "redhat" in os_release or "centos" in os_release:
         run("sudo yum install -y httpd")
         run("sudo systemctl start httpd")
         run("sudo systemctl enable httpd")
-    else:
-        print("no soporta")
-        exit(1)
 
+    else:
+        print("Distro no soportada")
+ 
 def crear_pagina():
     html = """
     <!DOCTYPE html>
@@ -30,19 +35,16 @@ def crear_pagina():
         <title>DevOps</title>
     </head>
     <body>
-        <h1>¡Hola!</h1>
+        <h1>¡Hola, bienvenido a el script de jair!</h1>
+        
     </body>
     </html>
     """
     with open("/tmp/index.html", "w", encoding="utf-8") as f:
         f.write(html)
-
     run("sudo mv /tmp/index.html /var/www/html/index.html")
 
 if __name__ == "__main__":
     instalar_apache()
     crear_pagina()
 
-# Otorgar permisos de ejecución al script
-    script_path = os.path.realpath(__file__)
-    os.chmod(script_path, os.stat(script_path).st_mode | stat.S_IEXEC)
